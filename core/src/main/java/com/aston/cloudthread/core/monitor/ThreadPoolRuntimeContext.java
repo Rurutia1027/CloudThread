084d21b81db30a86f67485ac382697768b1ba8ef
Rurutia1027 <rurutia1027@gmail.com>
2025-09-09 18:20:30 +0800

    feat: impl thread pool running time context metadata info

--- core/src/main/java/com/aston/cloudthread/core/monitor/ThreadPoolRuntimeContext.java
+++ core/src/main/java/com/aston/cloudthread/core/monitor/ThreadPoolRuntimeContext.java
@@ -1,4 +1,80 @@
 package com.aston.cloudthread.core.monitor;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Thread pool runtime context metadata holder class
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ThreadPoolRuntimeContext {
+    /**
+     * Thread pool context Unique Id (UID)
+     */
+    private String threadPoolUID;
+
+    /**
+     * Thread pool configured inner core thread size
+     */
+    private Integer corePoolSize;
+
+    /**
+     * Thread pool maximum threads can be hold
+     */
+    private Integer maximumPoolSize;
+
+    /**
+     * Thread pool current active thread number
+     */
+    private Integer currentPoolSize;
+
+    /**
+     * Thread pool active thread number
+     */
+    private Integer activePoolSize;
+
+    /**
+     * Largest pool size of thread pool
+     */
+    private Integer largestPoolSize;
+
+    /**
+     * Thread pool completed task counter
+     */
+    private Long completedTaskCount;
+
+    /**
+     * Thread pool block queue type
+     */
+    private String workQueueName;
+
+    /**
+     * Thread pool work queue capacity
+     */
+    private Integer workQueueCapacity;
+
+    /**
+     * Thread pool work queue size
+     */
+    private Integer workQueueSize;
+
+    /**
+     * Thread pool worker queue remaining capacity
+     */
+    private Integer workQueueRemainingCapacity;
+
+    /**
+     * Thread pool configured reject policy
+     */
+    private String rejectedHandlerName;
+
+    /**
+     * Thread pool reject policy handler invoke time counter
+     */
+    private Long rejectCount;
 }
