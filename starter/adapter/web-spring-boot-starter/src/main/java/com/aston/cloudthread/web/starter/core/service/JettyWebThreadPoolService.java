3441f5ff6c226e24b17e1ef7735da731f97090d9
Rurutia1027 <rurutia1027@gmail.com>
2025-09-15 16:13:36 +0800

    feat: impl two sub-classes of web servlet container bind with cloud thread pool

--- starter/adapter/web-spring-boot-starter/src/main/java/com/aston/cloudthread/web/starter/core/service/JettyWebThreadPoolService.java
+++ starter/adapter/web-spring-boot-starter/src/main/java/com/aston/cloudthread/web/starter/core/service/JettyWebThreadPoolService.java
@@ -13,37 +13,137 @@
  */
 package com.aston.cloudthread.web.starter.core.service;
 
+import cn.hutool.core.util.ReflectUtil;
+import com.aston.cloudthread.core.constant.Constants;
 import com.aston.cloudthread.web.starter.core.config.WebThreadPoolConfig;
 import com.aston.cloudthread.web.starter.core.enums.WebContainerEnum;
 import com.aston.cloudthread.web.starter.core.metric.WebThreadPoolMetrics;
 import com.aston.cloudthread.web.starter.core.snapshot.WebThreadPoolSnapshot;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.springframework.boot.web.embedded.jetty.JettyWebServer;
 import org.springframework.boot.web.server.WebServer;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 
+@Slf4j
 public class JettyWebThreadPoolService extends AbstractWebThreadPoolService {
     @Override
     protected Executor getWebServerInnerExecutor(WebServer webServer) {
-        return null;
+        return ((JettyWebServer) webServer).getServer().getThreadPool();
     }
 
     @Override
     public void updateThreadPool(WebThreadPoolConfig config) {
+        try {
+            QueuedThreadPool jettyExecutor = (QueuedThreadPool) webExecutorRef;
+            int originalCorePoolSize = jettyExecutor.getMinThreads();
+            int originalMaximumPoolSize = jettyExecutor.getMaxThreads();
+            long originalKeepAliveTime = jettyExecutor.getIdleTimeout();
 
+            if (config.getCorePoolSize() > originalMaximumPoolSize) {
+                jettyExecutor.setMaxThreads(config.getMaximumPoolSize());
+                jettyExecutor.setMinThreads(config.getCorePoolSize());
+            } else {
+                jettyExecutor.setMinThreads(config.getCorePoolSize());
+                jettyExecutor.setMaxThreads(config.getMaximumPoolSize());
+            }
+            jettyExecutor.setIdleTimeout(config.getKeepAliveTimeSeconds().intValue());
+
+            log.info("[Jetty] Changed web thread pool. corePoolSize: {}, maximumPoolSize: {}, keepAliveTime: {}",
+                    String.format(Constants.CHANGE_DELIMITER, originalCorePoolSize, config.getCorePoolSize()),
+                    String.format(Constants.CHANGE_DELIMITER, originalMaximumPoolSize, config.getMaximumPoolSize()),
+                    String.format(Constants.CHANGE_DELIMITER, originalKeepAliveTime, config.getKeepAliveTimeSeconds()));
+        } catch (Exception ex) {
+            log.error("Failed to modify the Jetty thread pool parameter.", ex);
+        }
     }
 
+    @SneakyThrows
     @Override
     public WebThreadPoolMetrics getMetrics() {
-        return null;
+        JettyThreadPoolInfo info = extractJettyThreadPoolInfo((QueuedThreadPool) webExecutorRef);
+
+        return WebThreadPoolMetrics.builder()
+                .corePoolSize(info.corePoolSize)
+                .maximumPoolSize(info.maximumPoolSize)
+                .keepAliveTimeSeconds(info.keepAliveTimeSeconds)
+                .workQueueName(info.queueName)
+                .workQueueSize(info.queueSize)
+                .workQueueRemainingCapacity(info.remainingCapacity)
+                .workQueueCapacity(info.queueCapacity)
+                .rejectedHandlerName(info.rejectedHandlerName)
+                .build();
     }
 
     @Override
     public WebThreadPoolSnapshot getRuntimeSnapshot() {
-        return null;
+        JettyThreadPoolInfo info = extractJettyThreadPoolInfo((QueuedThreadPool) webExecutorRef);
+
+        return WebThreadPoolSnapshot.builder()
+                .corePoolSize(info.corePoolSize)
+                .maximumPoolSize(info.maximumPoolSize)
+                .activePoolSize(info.activeCount)
+                .currentPoolSize(info.currentPoolSize)
+                .keepAliveTimeSeconds(info.keepAliveTimeSeconds)
+                .workQueueName(info.queueName)
+                .workQueueSize(info.queueSize)
+                .workQueueRemainingCapacity(info.remainingCapacity)
+                .workQueueCapacity(info.queueCapacity)
+                .rejectedHandlerName(info.rejectedHandlerName)
+                .build();
+    }
+
+    /**
+     * Extract common thread pool information from Jetty's QueuedThreadPool.
+     */
+    private JettyThreadPoolInfo extractJettyThreadPoolInfo(QueuedThreadPool jettyExecutor) {
+        int corePoolSize = jettyExecutor.getMinThreads();
+        int maximumPoolSize = jettyExecutor.getMaxThreads();
+        int activeCount = jettyExecutor.getBusyThreads();
+        int currentPoolSize = jettyExecutor.getThreads();
+        long keepAliveTimeSeconds = jettyExecutor.getIdleTimeout() * 1000L;
+
+        BlockingQueue<?> jobs = (BlockingQueue<?>) ReflectUtil.getFieldValue(jettyExecutor, "_jobs");
+        int blockingQueueSize = jettyExecutor.getQueueSize();
+        int remainingCapacity = jobs.remainingCapacity();
+        int queueCapacity = blockingQueueSize + remainingCapacity;
+        String rejectedExecutionHandlerName = "JettyRejectedExecutionHandler";
+
+        return new JettyThreadPoolInfo(
+                corePoolSize,
+                maximumPoolSize,
+                activeCount,
+                currentPoolSize,
+                keepAliveTimeSeconds,
+                jobs.getClass().getSimpleName(),
+                blockingQueueSize,
+                remainingCapacity,
+                queueCapacity,
+                rejectedExecutionHandlerName
+        );
     }
 
     @Override
     public WebContainerEnum getWebContainerType() {
-        return null;
+        return WebContainerEnum.JETTY;
     }
+
+    /**
+     * Simple DTO holding Jetty thread pool details for reuse across metrics/snapshot.
+     */
+    private record JettyThreadPoolInfo(
+            int corePoolSize,
+            int maximumPoolSize,
+            int activeCount,
+            int currentPoolSize,
+            long keepAliveTimeSeconds,
+            String queueName,
+            int queueSize,
+            int remainingCapacity,
+            int queueCapacity,
+            String rejectedHandlerName
+    ) {}
 }
--- starter/adapter/web-spring-boot-starter/src/main/java/com/aston/cloudthread/web/starter/core/service/TomcatWebThreadPoolService.java
+++ starter/adapter/web-spring-boot-starter/src/main/java/com/aston/cloudthread/web/starter/core/service/TomcatWebThreadPoolService.java
@@ -13,37 +13,148 @@
  */
 package com.aston.cloudthread.web.starter.core.service;
 
+import com.aston.cloudthread.core.constant.Constants;
 import com.aston.cloudthread.web.starter.core.config.WebThreadPoolConfig;
 import com.aston.cloudthread.web.starter.core.enums.WebContainerEnum;
 import com.aston.cloudthread.web.starter.core.metric.WebThreadPoolMetrics;
 import com.aston.cloudthread.web.starter.core.snapshot.WebThreadPoolSnapshot;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.threads.ThreadPoolExecutor;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
 import org.springframework.boot.web.server.WebServer;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class TomcatWebThreadPoolService extends AbstractWebThreadPoolService{
     @Override
     protected Executor getWebServerInnerExecutor(WebServer webServer) {
-        return null;
+        return ((TomcatWebServer) webServer).getTomcat()
+                .getConnector()
+                .getProtocolHandler()
+                .getExecutor();
     }
 
     @Override
     public void updateThreadPool(WebThreadPoolConfig config) {
+        try {
+            ThreadPoolExecutor tomcatExecutor = (ThreadPoolExecutor) webExecutorRef;
+            int originalCorePoolSize = tomcatExecutor.getCorePoolSize();
+            int originalMaximumPoolSize = tomcatExecutor.getMaximumPoolSize();
+            long originalKeepAliveTimeSecs = tomcatExecutor.getKeepAliveTime(TimeUnit.SECONDS);
 
+            if (config.getCorePoolSize() > originalMaximumPoolSize) {
+                tomcatExecutor.setMaximumPoolSize(config.getMaximumPoolSize());
+                tomcatExecutor.setCorePoolSize(config.getCorePoolSize());
+            } else {
+                tomcatExecutor.setCorePoolSize(config.getCorePoolSize());
+                tomcatExecutor.setMaximumPoolSize(config.getMaximumPoolSize());
+            }
+            tomcatExecutor.setKeepAliveTime(config.getKeepAliveTimeSeconds(), TimeUnit.SECONDS);
+
+            log.info("[Tomcat] Changed web thread pool. corePoolSize: {}, maximumPoolSize: {}, keepAliveTime: {}",
+                    String.format(Constants.CHANGE_DELIMITER, originalCorePoolSize, config.getCorePoolSize()),
+                    String.format(Constants.CHANGE_DELIMITER, originalMaximumPoolSize, config.getMaximumPoolSize()),
+                    String.format(Constants.CHANGE_DELIMITER, originalKeepAliveTimeSecs,
+                            config.getKeepAliveTimeSeconds()));
+        } catch (Exception ex) {
+            log.error("Failed to modify the Tomcat thread pool parameter.", ex);
+        }
     }
 
     @Override
     public WebThreadPoolMetrics getMetrics() {
-        return null;
+        TomcatThreadPoolInfo info = extractTomcatThreadPoolInfo((ThreadPoolExecutor) webExecutorRef);
+
+        return WebThreadPoolMetrics.builder()
+                .corePoolSize(info.corePoolSize)
+                .maximumPoolSize(info.maximumPoolSize)
+                .keepAliveTimeSeconds(info.keepAliveTimeSeconds)
+                .workQueueName(info.queueName)
+                .workQueueSize(info.queueSize)
+                .workQueueRemainingCapacity(info.remainingCapacity)
+                .workQueueCapacity(info.queueCapacity)
+                .rejectedHandlerName(info.rejectedHandlerName)
+                .build();
     }
 
     @Override
     public WebThreadPoolSnapshot getRuntimeSnapshot() {
-        return null;
+        TomcatThreadPoolInfo info = extractTomcatThreadPoolInfo((ThreadPoolExecutor) webExecutorRef);
+
+        return WebThreadPoolSnapshot.builder()
+                .corePoolSize(info.corePoolSize)
+                .maximumPoolSize(info.maximumPoolSize)
+                .activePoolSize(info.activeCount)
+                .completedTaskCount(info.completedTaskCount)
+                .largestPoolSize(info.largestPoolSize)
+                .currentPoolSize(info.currentPoolSize)
+                .keepAliveTimeSeconds(info.keepAliveTimeSeconds)
+                .workQueueName(info.queueName)
+                .workQueueSize(info.queueSize)
+                .workQueueRemainingCapacity(info.remainingCapacity)
+                .workQueueCapacity(info.queueCapacity)
+                .rejectedHandlerName(info.rejectedHandlerName)
+                .build();
+    }
+
+    /**
+     * Extract common thread pool information from Tomcat's ThreadPoolExecutor.
+     */
+    private TomcatThreadPoolInfo extractTomcatThreadPoolInfo(ThreadPoolExecutor executor) {
+        int corePoolSize = executor.getCorePoolSize();
+        int maximumPoolSize = executor.getMaximumPoolSize();
+        int activeCount = executor.getActiveCount();
+        long completedTaskCount = executor.getCompletedTaskCount();
+        int largestPoolSize = executor.getLargestPoolSize();
+        int currentPoolSize = executor.getPoolSize();
+        long keepAliveTimeSeconds = executor.getKeepAliveTime(TimeUnit.SECONDS);
+
+        BlockingQueue<?> queue = executor.getQueue();
+        int queueSize = queue.size();
+        int remainingCapacity = queue.remainingCapacity();
+        int queueCapacity = queueSize + remainingCapacity;
+        String rejectedHandlerName = executor.getRejectedExecutionHandler().getClass().getSimpleName();
+
+        return new TomcatThreadPoolInfo(
+                corePoolSize,
+                maximumPoolSize,
+                activeCount,
+                completedTaskCount,
+                largestPoolSize,
+                currentPoolSize,
+                keepAliveTimeSeconds,
+                queue.getClass().getSimpleName(),
+                queueSize,
+                remainingCapacity,
+                queueCapacity,
+                rejectedHandlerName
+        );
     }
 
     @Override
     public WebContainerEnum getWebContainerType() {
-        return null;
+        return WebContainerEnum.TOMCAT;
+    }
+
+    /**
+     * Simple DTO holding Tomcat thread pool details for reuse across metrics/snapshot.
+     */
+    private record TomcatThreadPoolInfo(
+            int corePoolSize,
+            int maximumPoolSize,
+            int activeCount,
+            long completedTaskCount,
+            int largestPoolSize,
+            int currentPoolSize,
+            long keepAliveTimeSeconds,
+            String queueName,
+            int queueSize,
+            int remainingCapacity,
+            int queueCapacity,
+            String rejectedHandlerName
+    ) {
     }
 }
