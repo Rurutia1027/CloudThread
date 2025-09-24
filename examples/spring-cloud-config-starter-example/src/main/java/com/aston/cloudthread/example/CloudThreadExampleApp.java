ab9ab7f1e2942809ae91dcd8a678f8ea6112d3bc
Rurutia1027 <rurutia1027@gmail.com>
2025-09-24 16:14:34 +0800

    add example codes for thread pool

--- examples/spring-cloud-config-starter-example/src/main/java/com/aston/cloudthread/example/CloudThreadExampleApp.java
+++ examples/spring-cloud-config-starter-example/src/main/java/com/aston/cloudthread/example/CloudThreadExampleApp.java
@@ -13,9 +13,11 @@
  */
 package com.aston.cloudthread.example;
 
+import com.aston.cloudthread.spring.base.enable.EnableCloudThread;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableCloudThread
 @SpringBootApplication
 public class CloudThreadExampleApp {
     public static void main(String[] args) {
