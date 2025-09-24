e32908397e946b04448042cbc9b29fc382d4ea05
Rurutia1027 <rurutia1027@gmail.com>
2025-09-24 15:58:51 +0800

    update codes for example usage of cloud thread pool

--- examples/spring-cloud-config-server/src/main/java/com/aston/cloudthread/config/server/SpringCloudConfigServerApplication.java
+++ examples/spring-cloud-config-server/src/main/java/com/aston/cloudthread/config/server/SpringCloudConfigServerApplication.java
@@ -11,10 +11,16 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
-package com.aston.cloudthread;
+package com.aston.cloudthread.config.server;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
     }
 }
