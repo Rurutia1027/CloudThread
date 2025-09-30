8a11a215b6d765e3f2c9bcb6b3793a9a1a7372aa
Rurutia1027 <rurutia1027@gmail.com>
2025-09-30 16:12:10 +0800

    remove the bootstrap configs tests from the main package, and then refine the ci pipeline scripts to let it enable dynamic reflection during test cases

--- examples/spring-cloud-config-starter-example/src/test/java/com/aston/cloudthread/example/tests/CloudThreadPoolTest.java
+++ examples/spring-cloud-config-starter-example/src/test/java/com/aston/cloudthread/example/tests/CloudThreadPoolTest.java
@@ -11,9 +11,10 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
-package com.aston.cloudthread.example.tests.configuration;
+package com.aston.cloudthread.example.tests;
 
 import com.aston.cloudthread.example.CloudThreadTestApp;
+import com.aston.cloudthread.example.tests.config.TestCloudThreadConfiguration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
